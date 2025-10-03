6b0fa0efeb6cc839492bbeea38718f3cda0f752f
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2025-10-03 20:05:47 +0100

    Create setup_cdn_edge.sh

---
+++ setup_cdn_edge.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+set -e
+
+# Step 2: Wait for external hostname
+while true
+do
+  echo "Killercoda service URL exposed to the Internet:  "
+  read HOST
+  if [[ -n "$HOST" ]]
+  then
+    echo "Backend hostname: $HOST"
+    break
+  fi
+done
+
+# Step 3: Deploy frontend reverse proxy
+BACKEND_HOST=$HOST envsubst '$BACKEND_HOST' < nginx-cdn-template.yaml | kubectl apply -f -
+echo "Reverse proxy deployed pointing to $BACKEND_HOST"
+
+kubectl get pod
+kubectl get svc
