1d5c2572ee923ada347864f3b506988526826c34
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2025-10-08 19:25:58 +0100

    Update setup_cdn_origin.sh

--- artifact/setup_cdn_origin.sh
+++ artifact/setup_cdn_origin.sh
@@ -15,11 +15,11 @@ pod=`kubectl get pod -o jsonpath='{.items[0].metadata.name}'`
 
 echo "kubectl exec $pod -- mkdir /var/www/html/hls/v0001"
 kubectl exec $pod -- mkdir /var/www/html/hls/v0001
-echo "kubectl cp Istio_ingessgateway_virtualservice_part01.mp4 $pod:/var/www/html/hls/v0001"
-kubectl cp Istio_ingessgateway_virtualservice_part01.mp4 $pod:/var/www/html/hls/v0001
+echo "kubectl cp Istio_ingressgateway_virtualservice_part01.mp4 $pod:/var/www/html/hls/v0001"
+kubectl cp Istio_ingressgateway_virtualservice_part01.mp4 $pod:/var/www/html/hls/v0001
 
-echo "kubectl exec $pod -- ffmpeg -i /var/www/html/hls/v0001/Istio_ingessgateway_virtualservice_part01.mp4 ..."
-kubectl exec $pod -- ffmpeg -i /var/www/html/hls/v0001/Istio_ingessgateway_virtualservice_part01.mp4 \
+echo "kubectl exec $pod -- ffmpeg -i /var/www/html/hls/v0001/Istio_ingressgateway_virtualservice_part01.mp4 ..."
+kubectl exec $pod -- ffmpeg -i /var/www/html/hls/v0001/Istio_ingressgateway_virtualservice_part01.mp4 \
 -codec:v libx264 -profile:v baseline -level 3.0 -s 640x360 -start_number 0 \
 -hls_time 6 -hls_list_size 0 -f hls /var/www/html/hls/v0001/playlist.m3u8
 kubectl exec $pod -- ls -al /var/www/html/hls/v0001
