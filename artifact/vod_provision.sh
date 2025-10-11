99d3dd66235b248977273adcbc75d9686d9405c4
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2025-10-12 00:54:15 +0100

    Update vod_provision.sh

--- artifact/vod_provision.sh
+++ artifact/vod_provision.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-vid=`awk -F: '{print $1}' /vod/new/summary.txt`
-file=`awk -F: '{print $2}' /vod/new/summary.txt`
+vodnew=$1
+if [ -z "$vodnew" ]
+then
+  vodnew="/vod/new"
+fi
+
+vidpath=`echo $vodnew/*.html`
+vidfile=`basename $vidpath`
+vid=${vidfile%%.*}
+mp4path=`echo $vodnew/*.mp4`
+mp4file=`basename $mp4path`
 
 if [ -n "$vid" ]
 then
@@ -9,18 +18,18 @@ then
   mkdir /var/www/html/hls/$vid
 fi
 
-if [ -f "/vod/new/$file" ] 
+if [ -f "$mp4path" ] 
 then
-  echo "cp /vod/new/$file /var/www/html/hls/$vid"
-  cp /vod/new/$file /var/www/html/hls/$vid
-  echo "ffmpeg -i /var/www/html/hls/v0001/$vid/$file ..."
-  ffmpeg -i /var/www/html/hls/v0001/$vid/$file -codec:v libx264 -profile:v baseline -level 3.0 -s 640x360 \
-  -start_number 0 -hls_time 6 -hls_list_size 0 -f hls /var/www/html/hls/v0001/playlist.m3u8
-  ls -al /var/www/html/hls/v0001/$vid
+  echo "cp $mp4path /var/www/html/hls/$vid"
+  cp $mp4path /var/www/html/hls/$vid
+  echo "ffmpeg -i /var/www/html/hls/v0001/$vid/$mp4file ..."
+  ffmpeg -i /var/www/html/hls/$vid/$file -codec:v libx264 -profile:v baseline -level 3.0 -s 640x360 \
+  -start_number 0 -hls_time 6 -hls_list_size 0 -f hls /var/www/html/hls/$vid/playlist.m3u8
+  ls -al /var/www/html/hls/$vid
 fi
 
-if [ -f "/vod/new/$vid.html" ] 
+if [ -f "$vodnew/$vid.html" ] 
 then
-  echo "cp /vod/new/$vid.html /var/www/html"
+  echo "cp $vodnew/$vid.html /var/www/html"
   cp /vod/new/$vid.html /var/www/html
 fi
