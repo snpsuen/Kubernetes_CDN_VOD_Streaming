8b6641b6f1580d4358ae4a8d4b862319d1223ffe
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2025-10-13 01:17:57 +0100

    Update setup_cdn_origin.sh

--- artifact02/setup_cdn_origin.sh
+++ artifact02/setup_cdn_origin.sh
@@ -4,9 +4,12 @@ echo "kubectl create -f https://raw.githubusercontent.com/snpsuen/Kubernetes_CDN
 kubectl create -f https://raw.githubusercontent.com/snpsuen/Kubernetes_CDN_VOD_Streaming/refs/heads/main/artifact02/nginx-hls.yaml
 
 mkdir -p /root/vod/new
+ssh node01 mkdir -p /root/vod/new
 git clone https://github.com/snpsuen/Kubernetes_CDN_VOD_Streaming
 cp Kubernetes_CDN_VOD_Streaming/artifact/*.mp4 /root/vod/new
 cp Kubernetes_CDN_VOD_Streaming/artifact02/*.html /root/vod/new
+scp Kubernetes_CDN_VOD_Streaming/artifact/*.mp4 node01:/root/vod/new
+scp Kubernetes_CDN_VOD_Streaming/artifact02/*.html node01:/root/vod/new
 
 echo "kubectl create -f https://raw.githubusercontent.com/snpsuen/Kubernetes_CDN_VOD_Streaming/refs/heads/main/artifact02/vod_provision_job.yaml"
 kubectl create -f https://raw.githubusercontent.com/snpsuen/Kubernetes_CDN_VOD_Streaming/refs/heads/main/artifact02/vod_provision_job.yaml
